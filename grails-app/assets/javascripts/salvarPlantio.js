257ab6c0b38e830a622519fcf628d2a9cdfc1b20
MrPontas <guipontarolo@gmail.com>
2021-09-09 18:57:19 -0300

    Comentado salvarPlantio.js

--- grails-app/assets/javascripts/salvarPlantio.js
+++ grails-app/assets/javascripts/salvarPlantio.js
@@ -1,9 +1,9 @@
-function salvarPlantio(){
-    $.ajax({
-        type: "POST",
-        url: ${createLink(controller:'selecionado', action:'setPlantioSelecionado')},
-        data: $("#formSelecionado").serialize()
-        // success: success,
-        // dataType: dataType
-    });
-}
+// function salvarPlantio(){
+//     $.ajax({
+//         type: "POST",
+//         url: ${createLink(controller:'selecionado', action:'setPlantioSelecionado')},
+//         data: $("#formSelecionado").serialize()
+//         // success: success,
+//         // dataType: dataType
+//     });
+// }
